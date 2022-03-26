0b200370b518aec7c483e266d9c42f1a1b012310
sakme <alan@akme.us>
2022-03-26 07:57:48 -0400

    added friend button and through form

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,6 +4,8 @@ import { QUERY_THOUGHTS, QUERY_ME_BASIC } from '../utils/queries';
 import ThoughtList from '../components/ThoughtList';
 import FriendList from '../components/FriendList';
 import Auth from '../utils/auth';
+import ThoughtForm from '../components/ThoughtForm';
+
 
 const Home = () => {
   const { loading, data } = useQuery(QUERY_THOUGHTS);
@@ -14,20 +16,25 @@ const Home = () => {
   return (
     <main>
       <div className='flex-row justify-space-between'>
-      <div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
-        {loading ? (
-          <div>Loading...</div>
-        ) : (
-          <ThoughtList thoughts={thoughts} title="Some Feed for Thought(s)..." />
+        {loggedIn && (
+          <div className="col-12 mb-3">
+            <ThoughtForm />
+          </div>
         )}
-      </div>
-      {loggedIn && userData ? (
-        <div className="col-12 col-lg-3 mb-3">
-          <FriendList
-            username={userData.me.username}
-            friendCount={userData.me.friendCount}
-            friends={userData.me.friends}
-          />
+        <div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
+          {loading ? (
+            <div>Loading...</div>
+          ) : (
+            <ThoughtList thoughts={thoughts} title="Some Feed for Thought(s)..." />
+          )}
+        </div>
+        {loggedIn && userData ? (
+          <div className="col-12 col-lg-3 mb-3">
+            <FriendList
+              username={userData.me.username}
+              friendCount={userData.me.friendCount}
+              friends={userData.me.friends}
+            />
         </div>
       ) : null}
       </div>
